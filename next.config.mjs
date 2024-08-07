dc4f8d3ce9e527f29bf74ce031c0bfcb36d715e3
Alessio Saltarin <alessiosaltarin@gmail.com>
2024-08-07 12:20:00 +0200

    Permissive CSP

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';"
+                    }
+                ],
+            },
+        ]
+    },
+};
 
 export default nextConfig;
