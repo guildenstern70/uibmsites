fe8d4e377775eae077ca055c86a16693eb8af2ac
Alessio Saltarin <alessiosaltarin@gmail.com>
2024-09-06 09:13:00 +0200

    New Figma

--- app/page.js
+++ app/page.js
@@ -70,20 +70,29 @@ export default function Home()
                     <h2 className={styles.title}>FIGMAs</h2>
                     <ul>
                         <li><a
-                            href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=2015%3A312660&node-id=8002-198518&t=pKUos7TMInhAezID-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=8002%3A198518&show-proto-sidebar=1">Parti
-                            Comuni</a></li>
+                            href="https://www.figma.com/design/BkWe7ggTQoJkan3V569mMh/DEV-Protos-show-v7-on-draft-team-vv2.2">
+                            Consegna Settembre #1</a></li>
                         <li><a
-                            href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=6001%3A344249&node-id=6001-344252&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A344250&show-proto-sidebar=1">Parti
-                            Specifiche</a></li>
-                        <li><a
-                            href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=6001%3A209006&node-id=6001-209981&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A209981&show-proto-sidebar=1">Marchi
-                            d&apos;Impresa</a></li>
-                        <li><a
-                            href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=4152%3A187029&node-id=6001-197659&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A198586&show-proto-sidebar=1">Brevetti</a>
+                            href="https://www.figma.com/design/qcFMkpUGElm13Hi3RKfEaj/DEV-Protos-show-v8 ">Consegna Settembre #2</a></li>
+                        <li>Oldies:
+                            <ul className={styles.nestedList}>
+                                <li><a
+                                    href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=2015%3A312660&node-id=8002-198518&t=pKUos7TMInhAezID-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=8002%3A198518&show-proto-sidebar=1">Parti
+                                    Comuni</a></li>
+                                <li><a
+                                    href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=6001%3A344249&node-id=6001-344252&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A344250&show-proto-sidebar=1">Parti
+                                    Specifiche</a></li>
+                                <li><a
+                                    href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=6001%3A209006&node-id=6001-209981&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A209981&show-proto-sidebar=1">Marchi
+                                    d&apos;Impresa</a></li>
+                                <li><a
+                                    href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=4152%3A187029&node-id=6001-197659&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A198586&show-proto-sidebar=1">Brevetti</a>
+                                </li>
+                                <li><a
+                                    href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=6001%3A422803&node-id=6001-422806&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A422804">Dati
+                                    Identificativi</a></li>
+                            </ul>
                         </li>
-                        <li><a
-                            href="https://www.figma.com/proto/oAhCO0JsyBDT1Rpqyh6IFu/DEV-Protos-show-v7-on-draft-team-vv2?page-id=6001%3A422803&node-id=6001-422806&t=DEQjnAVfVCt5jM4n-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=6001%3A422804">Dati
-                            Identificativi</a></li>
                     </ul>
                 </div>
             </div>
